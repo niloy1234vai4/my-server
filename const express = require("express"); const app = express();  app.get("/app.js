c130e8b8982fb7cd2ddcca497132afbcb6db4af4
niloy1234vai4 <niloy1234vai4@gmail.com>
2026-02-10 17:47:23 +0600

    Update app.js

--- const express = require("express"); const app = express();  app.get("/app.js
+++ const express = require("express"); const app = express();  app.get("/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.send("Server is running ✅");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("Running on port", port));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
